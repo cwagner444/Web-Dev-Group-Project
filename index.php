62a53d48e7ed193e24b1306a414ee799da9b9773
quintinmp <100251509+quintinmp@users.noreply.github.com>
2024-03-23 12:08:28 -0600

    added all pages to directory

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <a href="stylesheet" src="content/style.css"></a>
+    <script src="content/script.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
